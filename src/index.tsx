ba2af478d46d6d5ffb9a559a44ed741b9b9e108b
Paradox1st <paradox1st@pm.me>
2023-01-06 08:25:02 -0500

    changed index.js into typescript index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <h1>Hello World!</h1>
+  </React.StrictMode>
+);
