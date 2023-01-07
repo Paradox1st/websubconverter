b53b5d3e354d42df7a100a7beb101dbf68abc2d4
paradox <paradox1st@pm.me>
2023-01-06 20:10:44 -0500

    render with App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+class App extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
-root.render(<h1>Hello World!</h1>);
+root.render(<App />);
